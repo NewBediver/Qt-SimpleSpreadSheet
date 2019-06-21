e18567d783dbf2b7b96f9bf97532f101c33f94dd
NewBediver <51848889+NewBediver@users.noreply.github.com>
2019-06-22 08:39:11 +1000

    Create simple representation.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,9 +4,121 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    table = new QTableWidget();
+    table->setColumnCount(30);
+    table->setRowCount(1000);
+    setCentralWidget(table);
+
+    createActions();
+    createMenues();
+    createContextMenu();
+    createToolBars();
+    createStatusBar();
+
+    readSettings();
+
+    setCurrentFile("");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete table;
+}
+
+// Private methods
+void MainWindow::createActions()
+{
+
+}
+
+void MainWindow::createMenues()
+{
+
+}
+
+void MainWindow::createContextMenu()
+{
+
+}
+
+void MainWindow::createToolBars()
+{
+
+}
+
+void MainWindow::createStatusBar()
+{
+
+}
+
+void MainWindow::readSettings()
+{
+
+}
+
+void MainWindow::setCurrentFile(const QString &fileName)
+{
+
+}
+
+// Private slots
+void MainWindow::newFile()
+{
+
+}
+
+void MainWindow::open()
+{
+
+}
+
+bool MainWindow::save()
+{
+    return true;
+}
+
+bool MainWindow::saveAs()
+{
+    return true;
+}
+
+bool MainWindow::find()
+{
+    return true;
+}
+
+void MainWindow::goToCell()
+{
+
+}
+
+void MainWindow::sort()
+{
+
+}
+
+void MainWindow::about()
+{
+
+}
+
+void MainWindow::openRecentFile()
+{
+
+}
+
+void MainWindow::updateStatusBar()
+{
+
+}
+
+void MainWindow::spreadSheetModified()
+{
+
+}
+
+void MainWindow::closeEvent(QCloseEvent *)
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+#include <QAction>
+#include <QLabel>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,59 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent *);
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    bool find();
+    void goToCell();
+    void sort();
+    void about();
+    void openRecentFile();
+    void updateStatusBar();
+    void spreadSheetModified();
+
 private:
     Ui::MainWindow *ui;
+
+    // GUI
+    QTableWidget *table;
+    QLabel *locationLabel;
+    QLabel *formulaLabel;
+    QStringList recentFile;
+    QString curFile;
+
+    enum {MaxRecentFiles = 5};
+    QAction *recentFileActions[MaxRecentFiles];
+    QAction *separatorAction;
+
+    QMenu *fileMenu;
+    QMenu *editMenu;
+
+    QToolBar *fileToolBar;
+    QToolBar *editToolBar;
+    QAction *newAction;
+    QAction *openAction;
+    QAction *aboutQtAction;
+
+    void createActions();
+    void createMenues();
+    void createContextMenu();
+    void createToolBars();
+    void createStatusBar();
+    void readSettings();
+    void writeSettings();
+    bool okToContinue();
+    bool loadFile(const QString &fileName);
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    void updateRecentFileActions();
+    QString strippedName(const QString &fullFileNAme);
+
 };
 
 #endif // MAINWINDOW_H
